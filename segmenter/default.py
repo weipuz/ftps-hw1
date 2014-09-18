bc4fe9f806e41e7416f8d7066e413b364b61b745
weipuz <zhaoweipu.tju@gmail.com>
2014-09-17 18:05:57 -0700

    Test for pdist model
    test commond added to show if program it working correctly

--- segmenter/default.py
+++ segmenter/default.py
@@ -25,19 +25,20 @@ class Pdist(dict):
 
     def __call__(self, key):
         if key in self: return float(self[key])/float(self.N)
-        #else: return self.missingfn(key, self.N)
-        elif len(key) == 1: return self.missingfn(key, self.N)
-        else: return None
+        #else: return self.missingfn(key, self.N)             #pw=1/N when no character find
+        elif len(key) == 1: return self.missingfn(key, self.N)  #pw=1/N when single character is missing 
+        else: return None                                       #pw=None when a character longer than 1 is missing 
 
 # the default segmenter does not use any probabilities, but you could ...
 Pw  = Pdist(opts.counts1w)
-
+print Pw(unicode('法正','utf-8'))
 old = sys.stdout
 sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
 # ignoring the dictionary provided in opts.counts
+# from io import open
 with open(opts.input) as f:
     for line in f:
         utf8line = unicode(line.strip(), 'utf-8')
         output = [i for i in utf8line]  # segmentation is one word per character in the input
-        print " ".join(output)
+      #  print " ".join(output)
 sys.stdout = old
